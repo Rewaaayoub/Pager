9fe16cba7fb303ac6b386f8940cf83c2f24e803d
Rewaa Ayoub <>
2020-09-27 20:18:13 +0300

    1

--- Segements/FirstViewController.swift
+++ Segements/FirstViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 import XLPagerTabStrip
 
 class FirstViewController: UIViewController,IndicatorInfoProvider {
+    var itemInfo: IndicatorInfo = "View"
+
+    init(itemInfo: IndicatorInfo) {
+        self.itemInfo = itemInfo
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return itemInfo
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Segements/ScondViewController.swift
+++ Segements/ScondViewController.swift
@@ -9,6 +9,21 @@
 import UIKit
 import XLPagerTabStrip
 class ScondViewController: UIViewController ,IndicatorInfoProvider{
+    
+    var itemInfo: IndicatorInfo = "View"
+
+    init(itemInfo: IndicatorInfo) {
+        self.itemInfo = itemInfo
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return itemInfo
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()

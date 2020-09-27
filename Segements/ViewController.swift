7216c899261a5db088bd39eed4a8700b2d197175
Rewaa Ayoub <>
2020-09-27 20:14:36 +0300

    first commit

---
+++ Segements/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segements
+//
+//  Created by Rewaa Ayoub on 9/27/20.
+//  Copyright © 2020 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
